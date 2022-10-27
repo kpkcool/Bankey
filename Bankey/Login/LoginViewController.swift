0ffbf6dbb7b980a2c1c3c6c61fc199f232007bc8
K Praveen Kumar <kpkcool@gmail.com>
2022-10-27 21:29:39 +0530

    programmatically creating a custom view - LoginView

---
+++ Bankey/Login/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by K Praveen Kumar on 27/10/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController{
+    private func style(){
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    private func layout(){
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1), // 1 = 8pts
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+        
+    }
+}
