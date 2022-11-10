05058c660491d47df2855a8251b77d7afef51cdc
K Praveen Kumar <kpkcool@gmail.com>
2022-11-10 21:47:15 +0530

    unit testing - currency formatter

--- BankeyUnitTests/CurrencyFormatterTests.swift
+++ BankeyUnitTests/CurrencyFormatterTests.swift
@@ -21,4 +21,30 @@ class Test: XCTestCase{
     
     func testShouldBeVisible() throws {
     }
+    
+    func testBreakDollarsIntoCents(){
+        let result = formatter.breakIntoDollarsAndCents(926587.23)
+        XCTAssertEqual(result.0, "926,587")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+    func testDollarsFormatted() throws{
+        let result = formatter.dollarsFormatted(926587.23)
+        XCTAssertEqual(result, "$926,587.23")
+    }
+    
+    func testZeroDollarsFormatted() throws{
+        let result = formatter.dollarsFormatted(0.00)
+        XCTAssertEqual(result, "$0.00")
+    }
+    
+    func testDollarsFormattedWithCurrencySymbol() throws{
+        let locale = Locale.current
+        let currencySymbol = locale.currencySymbol!
+        
+        let result = formatter.dollarsFormatted(926587.23)
+        print("\(currencySymbol)")
+        XCTAssertEqual(result, "\(currencySymbol)926,587.23")
+    }
+
 }
